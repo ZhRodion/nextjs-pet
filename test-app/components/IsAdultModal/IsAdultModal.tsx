8fee73d409a88a3ef43129879cdaf73772dc043e
ZhRodion <jerdew.r@icloud.com>
2023-12-28 14:40:00 +0400

    Fix issues

--- test-app/components/IsAdultModal/IsAdultModal.tsx
+++ test-app/components/IsAdultModal/IsAdultModal.tsx
@@ -3,31 +3,43 @@
 import styles from '@/IsAdultModal/IsAdultModal.module.scss'
 import Cookies from 'js-cookie'
 import Link from 'next/link'
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 const IsAdultModal: FC = () => {
+	const [isShowModal, setIsShowModal] = useState(true)
+
 	const handleAgree = () => {
 		// Задать куку с именем 'isAdult' и значением 'true' на 60 дней
 		Cookies.set('isAdult', 'true', { expires: 60 })
+		setIsShowModal(false)
 	}
 
 	return (
-		<div className={styles.modalPageWrapper}>
-			<div className={styles.modalBody}>
-				<p className={styles.adultModalText}>
-					Вам <span className={styles.boldFont}>точно</span> исполнилось
-					<span className={styles.boldFont}> 18 </span> лет?
-				</p>
-				<div className={styles.adultModalBtnsWrapper}>
-					<button className={styles.adultAgreeBtn} onClick={handleAgree}>
-						Мне исполнилось 18 лет
-					</button>
-					<Link className={styles.adultRedirect} href='https://www.google.com/'>
-						<button className={styles.adultDisagreeBtn}>Мне еще нет 18</button>
-					</Link>
+		<>
+			{isShowModal && (
+				<div className={styles.modalPageWrapper}>
+					<div className={styles.modalBody}>
+						<p className={styles.adultModalText}>
+							Вам <span className={styles.boldFont}>точно</span> исполнилось
+							<span className={styles.boldFont}> 18 </span> лет?
+						</p>
+						<div className={styles.adultModalBtnsWrapper}>
+							<button className={styles.adultAgreeBtn} onClick={handleAgree}>
+								Мне исполнилось 18 лет
+							</button>
+							<Link
+								className={styles.adultRedirect}
+								href='https://www.google.com/'
+							>
+								<button className={styles.adultDisagreeBtn}>
+									Мне еще нет 18
+								</button>
+							</Link>
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	)
 }
 
--- test-app/components/ModalPortal/ModalPortal.tsx
+++ test-app/components/ModalPortal/ModalPortal.tsx
@@ -1,10 +1,17 @@
-'use effect'
+'use client'
 
 import IsAdultModal from '@/IsAdultModal/IsAdultModal'
 import Cookies from 'js-cookie'
+import { useEffect, useState } from 'react'
 import { createPortal } from 'react-dom'
 
 const ModalPortal = () => {
+	const [body, setBody] = useState<HTMLElement | null>(() => {
+		if (typeof document !== 'undefined') {
+			return document.body
+		}
+		return null
+	})
 	// Проверить, была ли установлена кука 'isAdult'
 	const isAdultCookieSet = Cookies.get('isAdult') === 'true'
 
@@ -13,7 +20,19 @@ const ModalPortal = () => {
 		return null
 	}
 
-	return <>{createPortal(<IsAdultModal />, document.body)}</>
+	useEffect(() => {
+		if (typeof document !== 'undefined') {
+			setBody(document.body)
+		}
+	}, [])
+
+	return (
+		<>
+			{!isAdultCookieSet &&
+				body !== null &&
+				createPortal(<IsAdultModal />, body)}
+		</>
+	)
 }
 
 export default ModalPortal
